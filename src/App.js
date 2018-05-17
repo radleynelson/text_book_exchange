3e6237d02b2c2a1ed0af3f4670019a1a8167cf33
Parker Brown <parkerbrown@Parkers-MBP-5.app.byu.edu>
2018-05-17 17:07:15 -0600

    added two buttons to home page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       <div className="App">
         <MuiThemeProvider>
           <NavBar/>
-          <Home/>
+          <Home />
         </MuiThemeProvider>
       </div>
     );
--- src/home.js
+++ src/home.js
@@ -3,16 +3,31 @@ import ReactDOM from 'react-dom';
 import RaisedButton from 'material-ui/RaisedButton';
 import {GridList, GridTile} from 'material-ui/GridList';
 
-class Home extends React.Component {
+const Styles = {
+    button: {
+        height: '25em',
+//        width: '25%',
+        margin: '2em',
+        
+    },
+    root: {
+        display: 'grid',
+        gridTemplate: 'auto auto/ auto auto',
+        gridColumnGap: '2em',
+}
+};
 
+class Home extends React.Component {
+    
   render(){
     return(
-      <section style={{display: 'grid'}}>
-        <div style={{backgroundColor: 'black', width: '400px', height: '400px'}}>
-        </div>
-        <div style={{backgroundColor: 'black', width: '400px', height: '400px'}}>
-        </div>
-      </section>
+        
+        <section style={Styles.root}>
+            <RaisedButton label="Full width" secondary={true} style={Styles.button} fullWidth={true} />
+        <RaisedButton label="Full width" secondary={true} style={Styles.button} fullWidth={true} />
+        </section>
+        
+        
   );
   }
 }
