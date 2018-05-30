a2c1fc865bfd5f4d0f9e509a9a0666b867b11fbb
Parker Brown <parkerbrown@Parkers-MacBook-Pro-5.local>
2018-05-30 16:44:05 -0600

    created cart and cartProduct components

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
-import { Heading, Hero, Container, Button } from 'react-bulma-components/full';
+import { Heading, Hero, Container, Button, Columns, Section, Box, Level } from 'react-bulma-components/full';
+import CartProduct from './CartProduct';
+ 
+
 
 export default class Cart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Listing: {},
+        }
+        
+    }
+    
+    componentDidMount(){
+    this.setState(({
+      Listing: {Title: 'Book 1', Author: 'Author 1', Id: 1, Price: '$20.00', Image: 'https://i.imgur.com/1w6UFY9.jpg', FullDescription:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.', DatePosted: '1:09 PM - 1 Jan 2018'}
+
+    }))
+    }
+    
+    handleDelete() {
+        alert('Functionality for deleting a cart item is coming soon!')
+    }
+    
     render() {
         return (
         <div>
+            <Section>
             <Hero color="info" gradient>
                 <Hero.Body>
                     <Container>
@@ -17,9 +40,17 @@ export default class Cart extends Component {
                     </Container>
                 </Hero.Body>
             </Hero>
-            <div>
-                <Button color="primary">Proceed to Checkout</Button>
-            </div>
+            </Section>
+            
+            <CartProduct Title={this.state.Listing.Title} Author={this.state.Listing.Author} ISBN={this.state.Listing.ISBN} Price={this.state.Listing.Price} />
+
+            <Section>
+            <Container>
+                <div>
+                    <Button color="primary">Proceed to Checkout</Button>
+                </div>
+            </Container>
+            </Section>
         </div>
 
 
---
+++ src/components/CartProduct.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import { Heading, Hero, Container, Button, Columns, Section, Box, Level, Media } from 'react-bulma-components/full';
+
+const Styles = {
+    image: {
+        height: '64',
+        width: '64',
+        background: '#e6e6e6',
+        
+    },
+    link: {
+        position: 'absolute',
+        bottom: '0',
+    }
+    
+}
+
+export default class CartProduct extends Component {
+    render() {
+        return(
+            <Section>
+            <Box>
+                <Media renderAs="nav">
+                    
+                    <Columns>
+                        <Columns.Column size={6}>
+                            <p>{this.props.Title}</p>
+                            <p>{this.props.Author}</p>
+                            <p>{this.props.ISBN}</p>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <Container>
+                                <span style={Styles.link}><a onClick={this.handleDelete}>Delete</a>
+                                       
+                                <a onClick={this.saveForLater}>Save for Later</a></span>
+                            </Container>
+                        </Columns.Column>
+                        <Columns.Column size={3}>
+                            <p>{this.props.Price}</p>
+                        </Columns.Column>
+                        <Columns.Column size={3}>
+                            <Media.Item renderAs="figure" position="right"><img style={Styles.image} alt="64x64" src="http://bulma.io/images/placeholders/128x128.png"/></Media.Item>
+                        </Columns.Column>
+                    </Columns>
+                
+                </Media>
+            </Box>
+        </Section>
+                      
+        );
+    }
+    
+          
+}
